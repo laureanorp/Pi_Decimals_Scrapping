1b33b8ea740296f8e7aee85f90d9d30b630d8295
Laureano Ruiz <laureano.ruiz.perez@gmail.com>
2020-08-27 12:35:11 +0100

    0.1.1
    Still not working, I have to rewrite a lot of the code

--- Pi_Decimals_Scrapping.py
+++ Pi_Decimals_Scrapping.py
@@ -4,54 +4,64 @@ import matplotlib.pyplot as plt
 import re
 
 
-# Extrae el HTMl, las wikitablas y las recorre una a una, fila a fila
+# Extrae el HTMl, las wikitablas y las recorre una a una y luego fila a fila
 # Luego extrae el primero y quinto valor (fecha y decimales de pi) y los añade a un diccionario
 # Constantes que indican la columna de la que se extraen los datos
-I1 = 4
-I = 0
-I2 = I1 + 1
+col1 = 0
+col2 = 4
+max_col = col2 + 1
 
 
 def extract_from_table(url):
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
     all_tables = soup.find_all(class_='wikitable')
-    clean_data = {}
+    data = {}
     for i in range(len(all_tables)):
         table = all_tables[i]
         rows = table.find_all('tr')
         for j in range(len(rows)):
             row = rows[j]
-            if not row.find_all('td') or len(row.find_all('td')) < I2:
+            if not row.find_all('td') or len(row.find_all('td')) < max_col:
                 first_cell = None
                 fifth_cell = None
             else:
-                first_cell = row.find_all('td')[I].get_text().replace('\n', '')
-                fifth_cell = row.find_all('td')[I1].get_text().replace('\n', '')
-                clean_data[first_cell] = fifth_cell
-    return clean_data
+                first_cell = row.find_all('td')[col1].get_text().replace('\n', '')
+                fifth_cell = row.find_all('td')[col2].get_text().replace('\n', '')
+                data[first_cell] = fifth_cell
+    return data
 
 
-# Convertir el diccionario en dos listas
+# Convertir el diccionario en dos listas con los numeros filtrados
 def dict_to_lists(dict):
     keys = list(dict.keys())
     values = list(dict.values())
     (keys, values) = zip(*dict.items())
     return keys, values
 
-# Eliminar todo lo que eno son fechas de cuatro dígitos
+
+# Eliminar lo que no son fechas de cuatro dígitos
 def leave_just_numbers(list):
-    re.findall(r'\d+', 'list')
+    four_digit_list = []
+    for i in list:
+        new_i = re.findall(r'\d{4}', i)
+        if new_i:
+            four_digit_list.append(new_i)
+    return four_digit_list
 
 
-# Imprimir los datos con una gráfica escalonada, WIP, desastre en los ejes
+# Imprimir los datos con una gráfica escalonada, WIP [desastre en los ejes]
 def plot_values(x, y):
-    # plt.plot(x, y)
     plt.step(x, y)
     plt.ylabel('Number of decimal digits')
     plt.xlabel('Year')
     plt.show()
 
-clean_data = extract_from_table('https://en.wikipedia.org/wiki/Chronology_of_computation_of_π')
-print(dict_to_lists(clean_data)[0])
-plot_values(dict_to_lists(clean_data)[0], dict_to_lists(clean_data)[1])
+
+data = extract_from_table('https://en.wikipedia.org/wiki/Chronology_of_computation_of_π')
+
+
+print((leave_just_numbers(dict_to_lists(data)[0])))
+print((dict_to_lists(data)[1]))
+
+# plot_values(dict_to_lists(data)[0], dict_to_lists(data)[1])
