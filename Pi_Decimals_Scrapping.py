133a469a01eae3d0d0f5e02b90a60bea3519a7cd
Laureano Ruiz <laureano.ruiz.perez@gmail.com>
2020-09-06 23:51:56 +0100

    fix(clean_decimals): remove commas from decimals, fix(plot_values): bigger size for the plot, feat(plot_values): first plot with real values, WIP

--- Pi_Decimals_Scrapping.py
+++ Pi_Decimals_Scrapping.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import re
 
@@ -34,27 +35,29 @@ def extract_from_table(url):
     return years, decimals
 
 
-# WIP: Simplificar todas las fechas a años de de 1-4 dígitos
-# Todo eliminar dias y meses si la fecha tiene 4 digitos, que es el año
+# WIP: Simplificar todas las fechas a solo el año, cogiendo solo el último número
 def clean_dates(raw_list):
-    four_digit_list = []
+    year_digit_list = []
     for i in raw_list:
         new_i = re.findall(r'\d+', i)
         if new_i:
-            four_digit_list.append(new_i)
-    return four_digit_list
+            new_i = new_i[-1]
+            year_digit_list.append(new_i)
+    return year_digit_list
 
 
-# Eliminar varios elementos de wikipedia que están entre corchetes y unas fórmulas
+# Eliminar varios elementos de wikipedia que están entre corchetes, las comas y varias fórmulas
 def clean_decimals(raw_list):
     clean_list = []
     for i in raw_list:
+        i = i.replace(',', '')
         clean_list.append(re.split("[=[:]+", i)[0])
     return clean_list
 
 
 # Imprimir los datos con una gráfica escalonada
 def plot_values(x, y):
+    mpl.rcParams["figure.figsize"] = [12, 8]
     plt.step(x, y)
     plt.ylabel('Number of decimal digits')
     plt.xlabel('Year')
@@ -63,12 +66,9 @@ def plot_values(x, y):
     plt.show()
 
 
-
 # Entragable inicial de un plot con valores inventados
-some_years = [150, 250, 1000, 1230, 1495, 1700, 1800, 2000, 2020]
-some_decimals = [1, 3, 5, 10, 20, 2000, 30000, 100000, 200000]
-plot_values(some_years, some_decimals)
-
+# some_years = [150, 250, 1000, 1230, 1495, 1700, 1800, 2000, 2020]
+#some_decimals = [1, 3, 5, 10, 20, 2000, 30000, 100000, 20000000]
 
 # Pruebas: Imprimir las fechas, los decimales y asegurarse de que cada lista sigue midiendo lo mismo.
 data = extract_from_table('https://en.wikipedia.org/wiki/Chronology_of_computation_of_π')
@@ -77,3 +77,6 @@ final_decimals = clean_decimals(data[1])
 print(final_dates)
 print(final_decimals)
 assert len(final_dates) == len(final_decimals)
+
+# Prueba de plot con valores reales a partir del año 150 d.C (para evitar "negativos")
+plot_values(final_dates[11:], final_decimals[11:])
